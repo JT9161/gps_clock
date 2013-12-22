ad1d8edc85cb7b7c996e53a392dc11bdb9cc7843
Jackson Tate <JT9161@gmail.com>
2013-12-21 17:43:16 -0800

    Start on timezone adjustment.

--- gps_clock/gps_clock.ino
+++ gps_clock/gps_clock.ino
@@ -3,7 +3,7 @@
 */
 
 #include <TinyGPS.h> //Parse NMEA input from GPS module
-//#include <Time.h> //Time sync control
+#include <Time.h> //Time sync control
 #include <SoftwareSerial.h> //Serial input control
 #include <glcd_Buildinfo.h> //LCD control
 #include <glcd.h> //LCD control
@@ -26,6 +26,7 @@ float latitude;
 float longitude;
 float course;
 int direction;
+signed int offset;
 
 TinyGPS gps; //Initialize library
 SoftwareSerial hard_serial(RXPIN, TXPIN); //Initialize library and define I/O pins
@@ -39,16 +40,20 @@ void setup() {
 	*/
 
 	//Set time zone: offset = direction * longitude * (24 / 360), where direction = 1 for east, -1 for west, and longitude is [-180,180]
-	gps.f_get_position(&latitude, &longitude);
-	course = gps.f_course();
-	if course >= 0
+	gps.f_get_position(&latitude, &longitude); //Get latitude and longitude, in degrees
+	course = gps.f_course(); //Get course in degrees
+	if (course >= 0) 	 //Determine if direction is east or west for timezone formula`
 		direction = 1;
 	else
 		direction = -1;
+	offset = direction * longitude * (24 / 360); //Set timezone
 }
 
 void loop() {
-	gps.crack_datetime(&utc_year, &utc_month, &utc_day, &utc_hour, &utc_minutes, &utc_second, &utc_hundredths);
+	gps.crack_datetime(&utc_year, &utc_month, &utc_day, &utc_hour, &utc_minutes, &utc_second, &utc_hundredths); //get time from GPS
+	setTime(utc_hour, utc_minutes, utc_second, utc_day, utc_month, utc_year); //pass time from GPS to time control
+	// adjustTime(); //apply calculated time zone 
+
 
 	// Send to display
 	GLCD.print(utc_second);
